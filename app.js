8c222225441a61d233e93ede88be80959f9f278f
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-03-16 16:26:18 -0600

    feat(utils) Add utils js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const fs = require('fs');
+const utils = require('./utils')
+const fs = require('fs')
 
 //write data in file sync (name, content)
 fs.writeFileSync('notes.txt','This file was created by Node.js.')
 
 //Append data to file sync
 fs.appendFileSync('notes.txt',' Written by Jorge')
+
+console.log(utils.add(1,4));
+console.log(utils.getNotes());
---
+++ utils.js
@@ -0,0 +1,15 @@
+//Function that returns the sum of 2 numbers
+const add = (a,b) => {
+    return a+b
+}
+
+//Function that returns a string value
+const getNotes = () => {
+    return "Your notes ..."
+}
+
+//Exports add, getNotes
+module.exports =  {
+    add,
+    getNotes
+}
