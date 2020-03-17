4228d1aca6e09adc212eb66a019cd9da027db452
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-03-16 21:34:58 -0600

    refactor(app) Refactor app

--- playground/index.js
+++ playground/index.js
@@ -1,4 +1,29 @@
+const utils = require('./utils')
+const fs = require('fs')
+const validator = require('validator')
+const chalk = require('chalk')
 const yargs = require('yargs')
+
+//write data in file sync (name, content)
+fs.writeFileSync('notes.txt','This file was created by Node.js.')
+
+//Append data to file sync
+fs.appendFileSync('notes.txt',' Written by Jorge')
+
+console.log(utils.add(1,4))
+
+console.log(utils.getNotes())
+
+console.log(validator.isEmail('joelias@example.com'))
+
+console.log(chalk.green('Success !'))
+
+//process.argv  get the input from the user in console
+//process.argv[2] example of index 2, that is the first user input after node and app.js path
+console.log(process.argv[2])
+
+//Customize yargs version
+yargs.version('1.1.0')
 
 //Create add command
 yargs.command({
@@ -49,3 +74,4 @@ yargs.command({
 })
 
 yargs.parse()
+//console.log(yargs.argv);
