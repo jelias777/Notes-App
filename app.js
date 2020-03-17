35578c94e47632e3ce8676e07b56b20b32daa9d8
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-03-16 20:22:20 -0600

    feat(yargs) Add a new command and modify version

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const utils = require('./utils')
 const fs = require('fs')
 const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
 
 //write data in file sync (name, content)
 fs.writeFileSync('notes.txt','This file was created by Node.js.')
@@ -20,3 +21,17 @@ console.log(chalk.green('Success !'))
 //process.argv  get the input from the user in console
 //process.argv[2] example of index 2, that is the first user input after node and app.js path
 console.log(process.argv[2])
+
+//Customize yargs version
+yargs.version('1.1.0')
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log('Adding a new note!')
+    }
+})
+
+console.log(yargs.argv);
